b091de07739569628bcb4073f23e7f8744422dd6
yinm5 <yinm5@ziroom.com>
2018-03-26 23:06:13 +0800

    service

---
+++ src/main/java/com/growing/sbmexc/service/AreaService.java
@@ -0,0 +1,9 @@
+package com.growing.sbmexc.service;
+
+/**
+ * @Author: yinm5
+ * @Version: 1.0
+ * @Date:2018/3/26
+ */
+public interface AreaService {
+}
---
+++ src/test/java/com/growing/sbmexc/dao/AreaDaoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+/**
+ * @Author: yinm5
+ * @Version: 1.0
+ * @Date:2018/3/26
+ */
+public class AreaDaoTest {
+
+}
